88eeefdbb70d4acd8420e917d35a31a88c3578f9
hyeonda02 <alceste951005@naver.com>
2024-12-15 23:24:52 +0900

    Refactor : QueryDSL 쿼리에 정렬 조건 추가

--- src/main/java/umc7/spring/repository/MissionRepositoryImpl.java
+++ src/main/java/umc7/spring/repository/MissionRepositoryImpl.java
@@ -1,18 +1,23 @@
+
 package umc7.spring.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import umc7.spring.domain.Mission;
 import umc7.spring.domain.QMission;
 import umc7.spring.domain.QStore;
 import umc7.spring.domain.mappings.QMissionComplete;
-import umc7.spring.repository.MissionRepositoryCustom;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -30,9 +35,14 @@ public class MissionRepositoryImpl implements MissionRepositoryCustom {
         }
         builder.and(missionComplete.member.id.eq(memberId));
 
+        OrderSpecifier<?> defaultSort = mission.createdAt.desc();
+        OrderSpecifier<?>[] sortOrders = getOrderSpecifiers(pageable, defaultSort);
+
+
         List<Mission> missions =  jpaQueryFactory.selectFrom(mission)
                 .leftJoin(mission.missionCompleteList, missionComplete)
                 .where(builder)
+                .orderBy(sortOrders)
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
@@ -74,4 +84,19 @@ public class MissionRepositoryImpl implements MissionRepositoryCustom {
         return new PageImpl<>(missions, pageable, total);
 
     }
+    private OrderSpecifier<?>[] getOrderSpecifiers(Pageable pageable, OrderSpecifier<?> defaultSort){
+        if (pageable.getSort().isSorted()) {
+            return pageable.getSort().stream()
+                    .map(order -> createOrderSpecifier(order))
+                    .toArray(OrderSpecifier[]::new);
+        }
+        return new OrderSpecifier<?>[] {defaultSort};
+    }
+    private OrderSpecifier<?> createOrderSpecifier(Sort.Order order) {
+        return new OrderSpecifier<>(
+                order.isAscending() ? Order.ASC : Order.DESC,
+                Expressions.path(LocalDateTime.class, mission, order.getProperty())
+        );
+    }
 }
+
